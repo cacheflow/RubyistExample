4c80efeea3e21de8cfe5f113f167fb2de2165dbc
Lex Alexander <l.alexander10@gmail.com>
2015-01-31 20:47:55 -0800

    first commit

---
+++ example.rb
@@ -0,0 +1,22 @@
+
+ticket = Object.new 
+
+def ticket.avaialbility?
+	false 
+end 
+
+
+a = Object.new 
+b = a 
+c = b 
+d = a 
+e = d 
+f = c 
+
+# puts "The id for a is #{a.object_id} and b is #{b.object_id} while d is #{d.object_id}"
+
+def ticket.me(a,b,*c)
+	puts "Hello senior how are you? #{c}"
+end 
+
+ ticket.me("lex", "tim", "chris", "bob", "sanchez")
